a056e96585ad7317fda4f7d081523f8d334ac9d5
Rilwan <rilwan.at@gmail.com>
2024-01-10 23:36:40 +0100

    added landing page logo
    - added landing page logo

--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -1,13 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
+import logo from '../../assets/logos/Logo Wordmark.png';
+
 export default function LandingPage() {
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
       
-      <Link to="/" style={{fontWeight: 'bold'}}>
+      <img className="w-56 h-20 object-scale-down" src={logo} alt="" />
+      {/* <Link to="/" style={{fontWeight: 'bold'}}>
         Africana Home
-      </Link>
+      </Link> */}
+      
 
     </div>
   );
